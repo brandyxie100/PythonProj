bc6c7ee4e1030c76e40fc0f292295979341eea7a
younanjiang010-creator <huijieyan010@gmail.com>
2026-05-30 15:10:58 +1200

    Create younan03.py

---
+++ FlyCode/flappybird/younan03.py
@@ -0,0 +1,10 @@
+#让人们宽容孔融可我怕柯克沃尔为客人人口特热天热扣扣
+import math
+
+radius = float(input('请输入圆的半径：'))
+pi = 3.1415926
+area = pi * radius ** 2     
+print(f'圆的面积是{area:.2f}')
+perimeter = 2 * pi * radius
+print(f'圆的周长是{perimeter:.2f}')
+3
